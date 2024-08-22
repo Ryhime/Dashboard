2c46d52afbdfc1fcb6acc18b1279fe1289523de1
Ryan <ryhime1@gmail.com>
2024-08-22 19:06:56 -0400

    Init Service

---
+++ Service/DashboardService.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify, request
+import pyshark
+import requests
+
+def getPackets(numPackets: int):
+    capture = pyshark.LiveCapture(interface='enp42s0')
+    capture.sniff(numPackets)
+
+    numPackets = 0
+    ipAddresses = {}
+    for packet in capture:
+        numPackets+=1
+        try:
+            ip = packet.ip.src
+            if (ip not in ipAddresses):
+                ipAddresses[ip] = 1
+            else:
+                ipAddresses[ip] += 1
+        except:
+            pass
+
+    return {
+        'ips': ipAddresses,
+        'numPackets': numPackets
+    }
+
+app = Flask(__name__)
+
+
+@app.route('/tasks', methods = ['GET'])
+def getTasks():
+    return jsonify('tasks', [])
+
+@app.route('/computer-stats', methods = ['GET'])
+def getComputerStats():
+    return jsonify('computer-stats', [])
+
+@app.route('/network-stats', methods = ['GET'])
+def getNetworkStats():
+    numPackets: int = int(request.args.get('numPackets'))
+    packetInfo = getPackets(numPackets)
+    return jsonify('network-stats', packetInfo)
+
+
+if (__name__ == '__main__'):
+    app.run(debug=True)
+
---
+++ Service/test_DashboardService.py
@@ -0,0 +1,4 @@
+from DashboardService import *
+
+def test_ExampleTest():
+    assert True == True
