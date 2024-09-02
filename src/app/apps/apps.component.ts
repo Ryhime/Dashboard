260b13d3847261b833e1c16cb1cd7e3319e2cc05
Ryan <ryhime1@gmail.com>
2024-09-01 21:15:27 -0400

    Basic Applications Panel

--- src/app/apps/apps.component.ts
+++ src/app/apps/apps.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './apps.component.scss'
 })
 export class AppsComponent {
-
+  onClickButtonUrl(url: string) {
+    window.open(url, '_blank');
+  }
 }
